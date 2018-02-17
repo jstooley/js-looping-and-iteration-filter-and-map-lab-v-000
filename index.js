11b4778bb58a0fc68f1551adf89c359f6127052a
Justin Tooley <jstooley823@gmail.com>
2018-02-17 04:01:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ function driverNamesWithRevenueOver(drivers, revenue){
   return driversWithRevenueOver(drivers, revenue).map(driver => {return driver.name});
 }
 
-function exactMatch(drivers, ){};
+function exactMatch(drivers, object){
+  return drivers.filter(driver => {
+        let matches = false;
+    for (const key in object){
+        matches = driver[key] === object[key]
+        }
+    return matches
+  });
+}
